d6f68d5d4cfbf5d7e801bd1813813ae971a7d887
srappaport <sarah.rappaport@greendot.org>
2018-07-27 02:22:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,10 @@
 class Owner
   attr_reader :name
-  attr_accessor :mood
+  attr_accessor :mood, :species
   def initialize(name)
     @name=name
     @mood="nervous"
+    @species = "human"
   end
   # code goes here
 
